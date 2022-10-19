1d87d7e765b4a24bdd55253d7787955de06e88c6
Minh Bui <ultraminh2709@gmail.com>
2022-10-18 18:13:40 -0700

    Added University Controller and Tests

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/UniversityController.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/UniversityController.java
@@ -3,6 +3,7 @@ package edu.ucsb.cs156.spring.backenddemo.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +11,16 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import edu.ucsb.cs156.spring.backenddemo.services.UniversityQueryService;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 
 
-@RestController
+@Api(description="University info from universities.hipolabs.com")
 @Slf4j
+@RestController
+@RequestMapping("/api/university")
 public class UniversityController {
     
 	ObjectMapper mapper = new ObjectMapper();
