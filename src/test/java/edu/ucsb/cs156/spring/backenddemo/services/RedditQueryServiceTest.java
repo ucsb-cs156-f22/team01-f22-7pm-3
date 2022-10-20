8409f1c914f2b36be436c698629cee164610f460
Ian Lai <76976018+ilai-ucsb@users.noreply.github.com>
2022-10-19 19:47:09 -0700

    Ian reddit query (#33)
    * IL - fixed Controller and Controller test
    
    * il - fixed mutations

--- src/test/java/edu/ucsb/cs156/spring/backenddemo/services/RedditQueryServiceTest.java
+++ src/test/java/edu/ucsb/cs156/spring/backenddemo/services/RedditQueryServiceTest.java
@@ -32,6 +32,7 @@ public class RedditQueryServiceTest {
         this.mockRestServiceServer.expect(requestTo(expectedURL))
                 .andExpect(header("Accept", MediaType.APPLICATION_JSON.toString()))
                 .andExpect(header("Content-Type", MediaType.APPLICATION_JSON.toString()))
+                .andExpect(header("User-Agent", "spring-boot:cs156-team01:f22 (by /u/Extra-Editor4414)"))
                 .andRespond(withSuccess(fakeJsonResult, MediaType.APPLICATION_JSON));
 
         String actualResult = redditQueryService.getJSON(subreddit);
